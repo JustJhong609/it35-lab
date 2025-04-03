670dee29ee6d5074504e313d8ac465f538b4c28b
JustJhong609 <20211199@nbsc.edu.ph>
2025-04-03 08:34:08 +0800

    Fix error and reoute

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Replace with your Supabase project credentials
-const SUPABASE_URL = "https://esjajkmgkthczrlyzwry.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzamFqa21na3RoY3pybHl6d3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MzQwMjUsImV4cCI6MjA1ODExMDAyNX0.FpzlUUYrjO7YelaP8icf8J7JvhFEhQk2vys2dxB2mxE";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
