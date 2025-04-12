41a6816f4568a6f23c0432ee1f959801b034be67
JustJhong609 <20211199@nbsc.edu.ph>
2025-04-12 12:40:33 +0800

    Updated the Login Register Page

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,81 +1,37 @@
-import {
+import { 
   IonButtons,
-  IonContent,
-  IonHeader,
-  IonMenuButton,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardSubtitle,
-  IonCardContent,
-  IonButton,
-} from "@ionic/react";
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
+} from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
+
 const Feed: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
+          <IonButtons slot='start'>
             <IonMenuButton></IonMenuButton>
           </IonButtons>
           <IonTitle>Feed</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Jhong Title</IonCardTitle>
-            <IonCardSubtitle>Owahaha</IonCardSubtitle>
-          </IonCardHeader>
-
-          <IonCardContent>Ako nagud ni naunsa diay mo .</IonCardContent>
-          <IonButton
-            fill="clear"
-            href="https://www.facebook.com/justjhongexd"
-            target="_blank"
-          >
-            Facebook
-          </IonButton>
-
-          <IonButton
-            fill="clear"
-            href="https://github.com/JustJhong609"
-            target="_blank"
-          >
-            Github
-          </IonButton>
-        </IonCard>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Jhong Title 2</IonCardTitle>
-            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-            Here's a small text description for the card content. Nothing more,
-            nothing less.
-          </IonCardContent>
-
-          <IonButton fill="clear">Action 1</IonButton>
-          <IonButton fill="clear">Action 2</IonButton>
-        </IonCard>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Ayaw kol</IonCardTitle>
-            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-            Here's a small text description for the card content. Nothing more,
-            nothing less.
-          </IonCardContent>
-
-          <IonButton fill="clear">Action 1</IonButton>
-          <IonButton fill="clear">Action 2</IonButton>
-        </IonCard>
+      <IonContent fullscreen>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Feed
+        </div>
+        <FeedContainer />
       </IonContent>
     </IonPage>
   );
